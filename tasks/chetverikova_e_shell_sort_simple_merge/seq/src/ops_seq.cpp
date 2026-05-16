8e54f69c523a1ea7f66963972302478b21dcf5ab
kate-cht <catherine.chet08@gmail.com>
2026-05-16 12:13:51 +0300

    ::ranges

--- tasks/chetverikova_e_shell_sort_simple_merge/seq/src/ops_seq.cpp
+++ tasks/chetverikova_e_shell_sort_simple_merge/seq/src/ops_seq.cpp
@@ -60,7 +60,7 @@ bool ChetverikovaEShellSortSimpleMergeSEQ::RunImpl() {
   ShellSort(right);
 
   std::vector<int> merged(left.size() + right.size());
-  std::merge(left.begin(), left.end(), right.begin(), right.end(), merged.begin());
+  std::ranges::merge(left, right, merged.begin());
   output = std::move(merged);
 
   return true;
